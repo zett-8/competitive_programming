46d0129ab947cdda0408fe818673cc427abdada4
toshiki.kamei <toshiki.kamei>
2019-10-29 16:50:52 +0900

    contest 144

---
+++ Level_C/Walk on Multiplication Table.py
@@ -0,0 +1,99 @@
+import subprocess
+import os
+import time
+
+
+import sys
+import math
+from copy import deepcopy
+from collections import deque, defaultdict
+from operator import mul
+from functools import reduce
+sys.setrecursionlimit(20000000)
+
+# input = sys.stdin.readline
+
+
+def main():
+    N = int(input())
+    min_s = 10 ** 12
+
+    for i in range(1, int(math.sqrt(N))+1):
+        if N % i == 0:
+            if min_s > i+(N/i):
+                min_s = i + (N/i)
+
+    print(min_s - 2)
+
+
+if __name__ == '__main__':
+    main()
+
+
+def test():
+    test_cases = [
+        [
+'''
+10
+''',
+'''
+5
+'''
+        ],
+        [
+'''
+50
+''',
+'''
+13
+'''
+        ],
+        [
+'''
+10000000019
+''',
+'''
+10000000018
+'''
+        ],
+    ]
+
+    wrong = 0
+
+    for i in range(len(test_cases)):
+        input_ = test_cases[i][0]
+        expect = test_cases[i][1].lstrip()
+
+        f = open('testcase', 'w', encoding='utf-8')
+        f.write(input_.lstrip())
+        f = open('testcase', 'r')
+
+        print('============ test case' + str(i+1) + ' ===========')
+        print('input:')
+        print(input_.lstrip())
+
+        print('expect:')
+        print(expect)
+
+        print('output:')
+        start = time.time()
+        result = subprocess.check_output(['python', '.'], stdin=f)
+        end = str(time.time() - start)
+        print(result.decode('utf-8'))
+
+        if expect == str(result.decode('utf-8')):
+            print('\033[32m' + 'SUCCESS!' + '\033[0m', 'time =', end[0: 5] + 's')
+        else:
+            wrong += 1
+            print('\033[31m' + 'SOMETHING IS WRONG...' + '\033[0m')
+
+        print('===================================')
+        print(' ')
+
+        f.close()
+
+        os.remove('testcase')
+
+    if not wrong:
+        print('\033[32m' + 'ALL CLEAR!!!' + '\033[0m')
+
