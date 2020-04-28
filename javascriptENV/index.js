01832c5a0c42d89bb1ce03151a682c2811368e06
Zett-8 <kame0008@gmail.com>
2020-04-28 16:44:18 +0200

    added javascript env for atcoder

---
+++ javascriptENV/index.js
@@ -0,0 +1,48 @@
+const request = require('request')
+const cheerio = require('cheerio')
+
+const test = () => {
+  request(url, (error, response, body) => {
+    if (error) return console.log(error)
+
+    const html = cheerio.load(body)
+    const test_input = []
+    const test_output = []
+
+    html('#task-statement >span >span:first-child >div.part pre').each((i,e) => {
+      i % 2 ? test_output.push(e.children[0].data) : test_input.push(e.children[0].data)
+    })
+
+    for (let [i, v] of test_input.entries()) {
+      console.log(`------ Test case ${i+1} ------`)
+
+      console.time()
+      main(v)
+      console.log(`\x1b[32m${test_output[i]}\x1b[0m`)
+      console.timeEnd()
+      console.log('----------------------------')
+
+      console.log()
+      console.log()
+    }
+  })
+}
+
+// TODO: set url for test
+const url = 'https://atcoder.jp/contests/abc164/tasks/abc164_a'
+
+// ======================================================================================================
+// ======================================================================================================
+
+
+const main = (input) => {
+  const [s, w] = input.trim().split(' ').map(v => parseInt(v, 10))
+
+  s <= w ? console.log('unsafe') : console.log('safe')
+}
+
+process.env.MYTEST ? test() : main(require('fs').readFileSync('/dev/stdin', 'utf8'))
+
+
+// ======================================================================================================
+// ======================================================================================================
