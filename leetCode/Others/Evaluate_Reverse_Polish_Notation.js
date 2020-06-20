4cedd6a740d2c3c25d6b0e19632c7aeaffaa3b5f
Zett-8 <kame0008@gmail.com>
2020-06-20 19:31:09 +0200

    solved

---
+++ leetCode/Others/Evaluate_Reverse_Polish_Notation.js
@@ -0,0 +1,55 @@
+/**
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Note:
+
+Division between two integers should truncate toward zero.
+The given RPN expression is always valid. That means the expression would always evaluate to a result and there won't be any divide by zero operation.
+Example 1:
+
+Input: ["2", "1", "+", "3", "*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: ["4", "13", "5", "/", "+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+Output: 22
+Explanation:
+  ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+ */
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+const evalRPN = (tokens) => {
+  const stack = []
+
+  for (let t of tokens) {
+    if (['+', '-', '*', '/'].includes(t)) {
+      const b = stack.pop()
+      const a = stack.pop()
+      if (t === '+') stack.push(a+b)
+      if (t === '-') stack.push(a-b)
+      if (t === '*') stack.push(a*b)
+      if (t === '/') stack.push(Math.trunc(a/b))
+    } else {
+      stack.push(parseInt(t))
+    }
+  }
+
+  return stack[0]
+}
