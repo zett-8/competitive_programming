a4a32a41d298ad66d3cef6a19fd47be178c1829b
Zett-8 <kame0008@gmail.com>
2020-07-15 18:40:25 +0200

    solved

---
+++ leetCode/Rotate_Image.js
@@ -0,0 +1,58 @@
+/**
+ You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Note:
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+
+Given input matrix =
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+rotate the input matrix in-place such that it becomes:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+Example 2:
+
+Given input matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+],
+
+rotate the input matrix in-place such that it becomes:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+ */
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+
+const rotate = (matrix) => {
+  for (let x=0; x<matrix.length; x++) {
+    for (let y=0; y<matrix.length; y++) {
+      if (x === y) break
+      [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]]
+    }
+  }
+
+  for (let i=0; i<matrix.length; i++) matrix[i] = matrix[i].reverse()
+}
