0133d434dd2003df16ae7cd36fa657f4ca543928
Zett-8 <kame0008@gmail.com>
2020-08-15 22:26:55 +0200

    solved

--- atcoder/javascriptENV/index.js
+++ atcoder/javascriptENV/index.js
@@ -29,14 +29,36 @@ const test = () => {
 }
 
 // TODO: set url for test
-const url = ''
+const url = 'https://atcoder.jp/contests/abc175/tasks/abc175_b'
 
 // ======================================================================================================
 // ======================================================================================================
 
 
 const main = (input) => {
-  const s = input.trim()
+  input = input.trim().split('\n')
+  const n = parseInt(input[0], 10)
+  const logs = input[1].split(' ').map(Number).sort((a, b) => a - b)
+
+  let ans = 0
+
+  const r = (idx, arr) => {
+    if (arr.length === 3) {
+      if (arr.length !== Array.from(new Set(arr)).length) return null
+      if (arr[2] >= arr[0] + arr[1]) return null
+
+      ans++
+      return null
+    }
+
+    if (idx > logs.length-1) return null
+
+    r(idx+1, arr)
+    r(idx+1, [...arr, logs[idx]])
+  }
+  r(0, [])
+
+  return console.log(ans)
 }
 
 process.env.MYTEST ? test() : main(require('fs').readFileSync('/dev/stdin', 'utf8'))
