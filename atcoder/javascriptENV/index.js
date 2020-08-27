7340c8739dba63612f8739113d7c9e4dc2e7244c
Zett-8 <kame0008@gmail.com>
2020-08-27 22:02:47 +0200

    solved

--- atcoder/javascriptENV/index.js
+++ atcoder/javascriptENV/index.js
@@ -29,14 +29,17 @@ const test = () => {
 }
 
 // TODO: set url for test
-const url = ''
+const url = 'https://atcoder.jp/contests/abc172/tasks/abc172_b'
 
 // ======================================================================================================
 // ======================================================================================================
 
 
 const main = (input) => {
-  const s = input.trim()
+  const [s, t] = input.trim().split('\n')
+  let count = 0
+  for (let i=0; i<s.length; i++) if (s[i] !== t[i]) count++
+  return console.log(count)
 }
 
 process.env.MYTEST ? test() : main(require('fs').readFileSync('/dev/stdin', 'utf8'))
