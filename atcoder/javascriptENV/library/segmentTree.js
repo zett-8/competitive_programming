92537b6603273eff65199f317f561aa2b954de27
Zett-8 <kame0008@gmail.com>
2020-10-24 17:42:02 +0200

    modified

--- atcoder/javascriptENV/library/segmentTree.js
+++ atcoder/javascriptENV/library/segmentTree.js
@@ -1,8 +1,4 @@
-/**
- * @param {number[]} nums
- */
-
-class NumArray {
+class segTree {
   constructor(arr) {
     this.inputArray = arr
     this.n = 1
@@ -40,3 +36,6 @@ class NumArray {
     return leftSum + rightSum
   }
 }
+
+// one line
+// class segTree { constructor(arr) { this.inputArray = arr; this.n = 1; while (this.n < this.inputArray.length) this.n *= 2; this.seg = new Array(this.n*2 - 1).fill(0); this.build() } build() { for (let i=0; i<this.inputArray.length; i++) this.seg[i+this.n-1] = this.inputArray[i]; for (let i=this.n-2; i>=0; i--) this.seg[i] = this.seg[i*2+1] + this.seg[i*2+2] } update(i, v) { const _i = i+this.n-1; const diff = v - this.seg[_i]; for (let x=_i; x>=0; x=Math.floor((x-1)/2)) this.seg[x] += diff } sumRange(a, b, k=0, l=this.n-1, r=this.seg.length-1) { if (r < a+this.n-1 || b+this.n-1 < l) return 0; if (a+this.n-1 <= l && r <= b+this.n-1) return this.seg[k]; const leftSum = this.sumRange(a, b, k*2+1, l, Math.floor((l+r)/2)); const rightSum = this.sumRange(a, b, k*2+2, Math.ceil((l+r)/2), r); return leftSum + rightSum }}
--- atcoder/javascriptENV/library/unionFind.js
+++ atcoder/javascriptENV/library/unionFind.js
@@ -28,3 +28,6 @@ class UnionFind {
     return this.findSet(this.p[x])
   }
 }
+
+// one line
+// class UnionFind { constructor(n) { this.p = new Array(n).fill(null).map((_, i) => i); this.height = new Array(n).fill(0); this.size = new Array(n).fill(1) } unite(a, b) { this.link(this.findSet(a), this.findSet(b)) } link(a, b) { if (a === b) return null; if (this.height[a] > this.height[b]) { this.p[b] = a; this.size[a] += this.size[b] } else { this.p[a] = b; this.size[b] += this.size[a]; if (this.height[a] === this.height[b]) this.height[b]++ }} findSet(x) { if (this.p[x] === x) return x ;return this.findSet(this.p[x]) }}
