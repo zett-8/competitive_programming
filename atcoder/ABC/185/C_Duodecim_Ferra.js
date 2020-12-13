cd5f9b3cfd6331cc64b3410098e698860cdddf90
Zett-8 <kame0008@gmail.com>
2020-12-13 13:48:38 +0100

    solved

--- atcoder/ABC/185/C_Duodecim_Ferra.js
+++ atcoder/ABC/185/C_Duodecim_Ferra.js
@@ -1,6 +1,6 @@
 const mod = 10**9 + 7
 const mlog = (v) => `${(v % mod + mod) % mod}`
-const nCr = (n, r, mod) => { n = BigInt(n); r = BigInt(r); mod = mod && BigInt(mod); let a = 1n, b = 1n; for (let i=0n; i<r; i+=1n) { a *= n-i; b *= r-i } return mod ? Number(a / b % mod) : Number(a / b) }
+const nCr = (n, r, mod) => { n = BigInt(n); r = BigInt(r); mod = mod && BigInt(mod); let a = 1n, b = 1n; for (let i=0n; i<r; i+=1n) { a *= n-i; b *= r-i } return mod ? Number(a / b % mod) : `${a / b}` }
 
 const main = (input) => {
   const n = Number(input.trim())
