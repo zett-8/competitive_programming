cd6e7929a8adef4701062f76963cd4843bcbb13a
Zett-8 <kame0008@gmail.com>
2020-12-22 00:30:48 +0100

    solved

--- atcoder/ABC/186/C_Unlucky_7.js
+++ atcoder/ABC/186/C_Unlucky_7.js
@@ -18,3 +18,30 @@ const main = (input) => {
 
   return ans
 }
+
+
+// O(n)
+
+const main = (input) => {
+  input = input.trim().split('\n')
+  const [H, W] = input[0].split(' ').map(Number)
+  const grid = input.slice(1)
+
+  for (let i=0; i<H; i++) grid[i] = grid[i].split(' ').map(Number)
+
+  let ans = 0
+  let min = grid[0][0]
+
+  for (let count=0, h=0; h<H; h++) {
+    for (let w=0; w<W; w++) {
+      if (grid[h][w] > min) ans += grid[h][w] - min
+      else {
+        ans += count * (min - grid[h][w])
+        min = grid[h][w]
+      }
+
+      count++
+    }
+  }
+  return ans
+}
