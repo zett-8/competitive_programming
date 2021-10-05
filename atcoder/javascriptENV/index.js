979b18ae53d95e0d3f52d3dfa51dc717fc52f729
Zett-8 <kame0008@gmail.com>
2021-10-05 21:02:10 +0200

    205

--- atcoder/javascriptENV/index.js
+++ atcoder/javascriptENV/index.js
@@ -4,11 +4,14 @@ const cheerio = require('cheerio')
 const test = () => {
   const test_input = [
     // 1
-    ``,
+    `3 2 4
+`,
     // 2
-    ``,
+    `-7 7 2
+`,
     // 3
-    ``,
+    `-8 6 3
+`,
     // 4
     ``,
     // 5
@@ -16,20 +19,20 @@ const test = () => {
   ]
   const test_output = [
     // 1
-    ``,
+    `>`,
     // 2
-    ``,
+    `=`,
     // 3
-    ``,
+    `<`,
     // 4
     ``,
     // 5
     ``,
   ]
-  
+
   for (let [i, v] of test_input.entries()) {
     if (!v) return
-  
+
     console.log(`------ Test case ${i + 1} ------`)
 
     console.time()
@@ -52,7 +55,15 @@ const mod = 10**9 + 7
 const mlog = (v) => `${(v % mod + mod) % mod}`
 
 const main = (input) => {
-  input = input.trim()
+  let [a, b, c] = input.trim().split(' ').map(Number)
+
+  if (c === 0) return '='
+
+  if (a < 0 && c % 2 === 0) a *= -1
+  if (b < 0 && c % 2 === 0) b *= -1
+
+  if (a === b) return '='
+  return a > b ? '>' : '<'
 }
 
 process.env.MYTEST
