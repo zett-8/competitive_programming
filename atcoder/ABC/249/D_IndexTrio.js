e550eada6e66e826e4b81803e75cce941e2957ad
Toshiki Kamei <kame0008@gmail.com>
2022-05-10 14:14:18 +0200

    solve 249 D

--- atcoder/ABC/249/D_IndexTrio.js
+++ atcoder/ABC/249/D_IndexTrio.js
@@ -6,12 +6,10 @@ const main = (input) => {
 
   let ans = 0
 
-  outer:
   for (let i=0; i<a.length; i++) {
     for (let j = 0; j < a.length; j++) {
-      if (a[i] * a[j] > a[a.length-1]) continue outer
-      if (count[a[i] * a[j]])
-        ans += count[a[i]] * count[a[j]] * count[a[i] * a[j]]
+      if (a[i] * a[j] > a[a.length-1]) break
+      if (count[a[i] * a[j]]) ans += count[a[i]] * count[a[j]] * count[a[i] * a[j]]
     }
   }
 
