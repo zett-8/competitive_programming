3bef33a48fa574fd05f0b6f3589f95648594b9c9
Toshiki Kamei <kame0008@gmail.com>
2022-06-09 16:59:35 +0200

    important 100 no.38

--- important100/DP/knapsack_DP/38-ALDS_10_C_LongestCommonSubsequence.py
+++ important100/DP/knapsack_DP/38-ALDS_10_C_LongestCommonSubsequence.py
@@ -11,12 +11,12 @@ def main():
     def lcs(x, y):
         dp = [[0]*(len(x)+1) for _ in range(len(y)+1)]
 
-        for i, a in enumerate(y):
-            for j, b in enumerate(x):
-                if a == b:
-                    dp[i+1][j+1] = dp[i][j] + 1
+        for i in range(1, len(y)+1):
+            for j in range(1, len(x)+1):
+                if y[i] == x[j]:
+                    dp[i][j] = dp[i-1][j-1] + 1
                 else:
-                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
 
         return dp[len(y)][len(x)]
 
